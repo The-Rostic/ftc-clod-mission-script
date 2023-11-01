8ec1735c4a2cda85dfaf8a4012d8f358662d2202
Rostyslav Vahanov <vahanov.rostyslav@gmail.com>
2023-11-01 18:55:40 +0200

    initial addition of files

---
+++ TheEnglishChannel/FTC-test.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System;
+using System.Collections;
+using maddox.game;
+using maddox.game.world;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
+using System.Diagnostics;
+using part;
+
+public class Mission : AMission
+{
+    public override void OnBattleStarted()
+    {
+        base.OnBattleStarted();
+
+    }
+
+    public override void OnPlaceEnter(Player player, AiActor actor, int placeIndex)
+    {
+        base.OnPlaceEnter(player, actor, placeIndex);
+    }
+
+    public override void OnPlaceLeave(Player player, AiActor actor, int placeIndex)
+    {
+        base.OnPlaceLeave(player, actor, placeIndex);
+    }
+
+    public override void OnActorDead(int missionNumber, string shortName, AiActor actor, List<DamagerScore> damages)
+    {
+        base.OnActorDead(missionNumber, shortName, actor, damages);
+    }
+
+    public override void OnAircraftCrashLanded(int missionNumber, string shortName, AiAircraft aircraft)
+    {
+        base.OnAircraftCrashLanded(missionNumber, shortName, aircraft);
+    }
+    public override void OnAircraftLanded(int missionNumber, string shortName, AiAircraft aircraft)
+    {
+        base.OnAircraftLanded(missionNumber, shortName, aircraft);
+    }
+    public override void OnTrigger(int missionNumber, string shortName, bool active)
+    {
+        base.OnTrigger(missionNumber, shortName, active);
+    }
+    public override void OnTickGame()
+    {
+
+    }
+
+}
