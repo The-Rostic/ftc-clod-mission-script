20f13dd30c7ff7839c41325f64e876478c1fdcf8
Rostyslav Vahanov <vahanov.rostyslav@gmail.com>
2023-11-04 22:39:17 +0200

    CKillDisusedPlanes redone! Now it kills just aircraft that was player controlled. Not the whole group.

--- TheEnglishChannel/FTC-test.cs
+++ TheEnglishChannel/FTC-test.cs
@@ -8,7 +8,7 @@ using System.ComponentModel;
 using System.Threading;
 using System.Diagnostics;
 using part;
-using maddox.GP;
+using maddox.GP; //-------------------
 
 //$include .\modules\CConfig.cs
 //$include .\modules\CKillDisusedPlanes.cs
@@ -16,6 +16,8 @@ using maddox.GP;
 
 public class Mission : AMission
 {
+    public bool DEBUG_MESSAGES = true;
+    
     public CKillDisusedPlanes m_KillDisusedPlanes = null;
 
     public override void OnBattleStarted()
@@ -23,11 +25,96 @@ public class Mission : AMission
         base.OnBattleStarted();
         m_KillDisusedPlanes = new CKillDisusedPlanes(this);
         CLog.Init(this);
-        CLog.Write("OnBattleStarted");
+        if (DEBUG_MESSAGES) CLog.Write("OnBattleStarted");
+        PrepareAirfields();
         // listen all the mission
-        this.MissionNumberListener = -1;
+        MissionNumberListener = -1;
+    }
 
-        //// Get list of all airfields on the map
+    public override void OnBattleStoped()
+    {
+        base.OnBattleStoped();
+        if (DEBUG_MESSAGES) CLog.Write("OnBattleStoped");
+        CLog.Close();
+    }
+
+    public override void OnPlaceEnter(Player player, AiActor actor, int placeIndex)
+    {
+        base.OnPlaceEnter(player, actor, placeIndex);
+        if (DEBUG_MESSAGES) CLog.Write("OnPlaceEnter player=" + player.Name() + " actor=" + actor.Name() + " placeIdx=" + placeIndex.ToString());
+    }
+
+    public override void OnPlaceLeave(Player player, AiActor actor, int placeIndex)
+    {
+        base.OnPlaceLeave(player, actor, placeIndex);
+        if (DEBUG_MESSAGES) CLog.Write("OnPlaceLeave player=" + player.Name() + " actor=" + actor.Name() + " placeIdx=" + placeIndex.ToString());
+        m_KillDisusedPlanes.OnPlaceLeave(player, actor, placeIndex);
+    }
+
+    public override void OnActorDead(int missionNumber, string shortName, AiActor actor, List<DamagerScore> damages)
+    {
+        base.OnActorDead(missionNumber, shortName, actor, damages);
+        if (DEBUG_MESSAGES) CLog.Write("OnActorDead " + shortName + " actor=" + actor.Name());
+    }
+
+    public override void OnAircraftTookOff(int missionNumber, string shortName, AiAircraft aircraft)
+    {
+        base.OnAircraftTookOff(missionNumber, shortName, aircraft);
+        if (DEBUG_MESSAGES) CLog.Write("OnAircraftTookOff " + shortName + " aircraft=" + aircraft.Name());
+    }
+
+    public override void OnAircraftCrashLanded(int missionNumber, string shortName, AiAircraft aircraft)
+    {
+        base.OnAircraftCrashLanded(missionNumber, shortName, aircraft);
+        if (DEBUG_MESSAGES) CLog.Write("OnAircraftCrashLanded " + shortName + " aircraft=" + aircraft.Name());
+    }
+    public override void OnAircraftLanded(int missionNumber, string shortName, AiAircraft aircraft)
+    {
+        base.OnAircraftLanded(missionNumber, shortName, aircraft);
+        if (DEBUG_MESSAGES) CLog.Write("OnAircraftCrashLanded " + shortName + " aircraft=" + aircraft.Name());
+    }
+    public override void OnTrigger(int missionNumber, string shortName, bool active)
+    {
+        base.OnTrigger(missionNumber, shortName, active);
+    }
+    public override void OnTickGame()
+    {
+
+    }
+
+
+    //
+    //  Custom functions
+    //
+
+    AiAirport[] airportsSortedByArmy = null;
+
+    public void PrepareAirfields()
+    {
+        // Get list of all airfields on the map
+        airportsSortedByArmy = GamePlay.gpAirports();
+        AiAirport aiAirport = null;
+        // Sort by army
+        for (int i = 0; i < airportsSortedByArmy.Length - 1; i++)
+        {
+            for (int j = i + 1; j < airportsSortedByArmy.Length; j++)
+            {
+                if (airportsSortedByArmy[i].Army() > airportsSortedByArmy[j].Army())
+                {
+                    aiAirport = airportsSortedByArmy[i];
+                    airportsSortedByArmy[i] = airportsSortedByArmy[j];
+                    airportsSortedByArmy[j] = aiAirport;
+                }
+            }
+        }
+        if (DEBUG_MESSAGES) CLog.Write("Sorted list of airfields:");
+        for (int i = 0; i < airportsSortedByArmy.Length; i++)
+        {
+            if (DEBUG_MESSAGES) CLog.Write(airportsSortedByArmy[i].Name() + " army=" + airportsSortedByArmy[i].Army().ToString());
+        }
+        ////
+        //// Print list of airfields in mission
+        ////
         //for (int i = 0; i < GamePlay.gpAirports().Length; i++)
         //{
         //    AiAirport airport = GamePlay.gpAirports()[i];
@@ -39,56 +126,72 @@ public class Mission : AMission
         //        + " FieldR=" + airport.FieldR().ToString()
         //        );
         //}
-       }
-
-    public override void OnBattleStoped()
-    {
-        base.OnBattleStoped();
-        CLog.Write("OnBattleStoped");
-        CLog.Close();
     }
 
-    public override void OnPlaceEnter(Player player, AiActor actor, int placeIndex)
+    public bool IsAircraftAtFriendlyAirfield(AiAircraft Aircraft)
     {
-        base.OnPlaceEnter(player, actor, placeIndex);
-        CLog.Write("OnPlaceEnter player=" + player.Name() + " actor=" + actor.Name() + " placeIdx=" + placeIndex.ToString());
-    }
+        if (Aircraft == null)
+        { 
+            return false; 
+        }
+        bool aircraftIsOnTheGround = false;
+        if (!Aircraft.IsAirborne()) // Just spawen and never airborne.
+        {
+            aircraftIsOnTheGround = true;
+            if (DEBUG_MESSAGES) CLog.Write(Aircraft.Name() + " is NOT airborne.");
+        }
+        else // Important notice! Aircraft that airborne once stays airborne forever, even after landed.
+        {
+            double aircraftAGL = Aircraft.getParameter(part.ParameterTypes.Z_AltitudeAGL, -1);
+            double aircraftTAS = Aircraft.getParameter(part.ParameterTypes.Z_VelocityTAS, -1);
+            if (DEBUG_MESSAGES) CLog.Write(Aircraft.Name() + " is AGL=" + aircraftAGL.ToString() + "m and TAS=" + aircraftTAS.ToString() + "m/s");
+            if ((aircraftAGL < 5) && (aircraftTAS < 3.6))
+            {
+                aircraftIsOnTheGround = true;
+            }
+        }
 
-    public override void OnPlaceLeave(Player player, AiActor actor, int placeIndex)
-    {
-        base.OnPlaceLeave(player, actor, placeIndex);
-        CLog.Write("OnPlaceLeave player=" + player.Name() + " actor=" + actor.Name() + " placeIdx=" + placeIndex.ToString());
-        m_KillDisusedPlanes.OnPlaceLeave(player, actor, placeIndex);
+        if (aircraftIsOnTheGround)
+        {
+            Point3d aircraftPos = Aircraft.Pos();
+            int aircraftArmy = Aircraft.Army();
+            AiAirport[] airports = GamePlay.gpAirports();
+            AiAirport airportFriendly, airportNeutral;
+            Point3d airportNeutralPos;
+            for (int i = 0; i < airports.Length; i++)
+            {
+                airportNeutral = airports[i];
+                // get neutral airfileds with friendly spawn area airports nearby...
+                if (airportNeutral.Army() == 0)
+                {
+                    airportNeutralPos = airportNeutral.Pos();
+                    for (int j = 0; j < airports.Length; j++)
+                    {
+                        if (i == j) continue;
+                        airportFriendly = airports[j];
+                        if (airportFriendly.Army() == aircraftArmy)
+                        {
+                            Point3d airportFriendlyPos = airportFriendly.Pos();
+                            if (airportNeutralPos.distanceLinf(ref airportFriendlyPos) < airportNeutral.CoverageR())
+                            {
+                                // Ok, this neutral airport contain friendly spawn area airport. Check if we are in this neutral airport radius
+                                double distToAirportNeutral = airportNeutralPos.distanceLinf(ref aircraftPos);
+                                if (distToAirportNeutral < airportNeutral.CoverageR())
+                                {
+                                    if (DEBUG_MESSAGES) CLog.Write(Aircraft.Name() + " is on friendly airfiled " + airportNeutral.Name() + " distance " + distToAirportNeutral.ToString());
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (DEBUG_MESSAGES) CLog.Write(Aircraft.Name() + " is abbandoned on the ground.");
+        }
+        else
+        {
+            if (DEBUG_MESSAGES) CLog.Write(Aircraft.Name() + " is airborne.");
+        }
+        return false;
     }
-
-    public override void OnActorDead(int missionNumber, string shortName, AiActor actor, List<DamagerScore> damages)
-    {
-        base.OnActorDead(missionNumber, shortName, actor, damages);
-    }
-
-    /*public override void OnAircraftTookOff(int missionNumber, string shortName, AiAircraft aircraft)
-    {
-        base.OnAircraftTookOff(missionNumber, shortName, aircraft);
-        CLog.Write("OnAircraftTookOff " + shortName + " aircraft=" + aircraft.Name());
-    }*/
-
-    public override void OnAircraftCrashLanded(int missionNumber, string shortName, AiAircraft aircraft)
-    {
-        base.OnAircraftCrashLanded(missionNumber, shortName, aircraft);
-        CLog.Write("OnAircraftCrashLanded " + shortName + " aircraft=" + aircraft.Name());
-    }
-    public override void OnAircraftLanded(int missionNumber, string shortName, AiAircraft aircraft)
-    {
-        base.OnAircraftLanded(missionNumber, shortName, aircraft);
-        CLog.Write("OnAircraftCrashLanded " + shortName + " aircraft=" + aircraft.Name());
-    }
-    public override void OnTrigger(int missionNumber, string shortName, bool active)
-    {
-        base.OnTrigger(missionNumber, shortName, active);
-    }
-    public override void OnTickGame()
-    {
-
-    }
-
 }
--- TheEnglishChannel/modules/CKillDisusedPlanes.cs
+++ TheEnglishChannel/modules/CKillDisusedPlanes.cs
@@ -34,25 +34,27 @@ using System;
 using System.Threading;
 using maddox.game;       /// AMission
 using maddox.game.world; /// AiActor
-using maddox.GP;
+using maddox.GP;//-----------------------------
 
 public class CKillDisusedPlanes {
     /// Set to true to see debug messages in server messages
     public bool DEBUG_MESSAGES = true;
-    
+
+    // number of seconds to wait between damage and destroy of aircraft on friendly airfield
+    protected const int TIMEOUT_ATFRIENDLYBASE = 1;
+
+    // number of seconds to wait between damage and destroy airborne aircraft
+    protected const int TIMEOUT_ABANDONED = 60; // < 0 means do not destroy aircraft
+
     protected AMission m_Mission = null;
-    /// number of seconds to wait between damage and destroy just spawned or landed aircraft
-    protected int m_iSecondsUntilRemoveAtFriendlyBase = 1;
-    /// number of seconds to wait between damage and destroy airborne aircraft
-    protected int m_iSecondsUntilRemoveAbandoned = 9999;
 
     /// <summary>
     /// Constructor
     /// </summary> 
     /// <param name="Mission">The Mission (use 'this')</param>
-    public CKillDisusedPlanes(AMission Mission)
+    public CKillDisusedPlanes(AMission mission)
     {
-        m_Mission = Mission;
+        m_Mission = mission;
     }
     /// <summary>
     /// Make sure that the plane a player has left cannot be regained
@@ -67,165 +69,106 @@ public class CKillDisusedPlanes {
         });
     }
     /// <summary>
-    /// Destroy aircraft
-    /// </summary> 
-    protected void DestroyPlane(AiAircraft Aircraft)
-    {
-        if (Aircraft != null)
-        {
-            Aircraft.Destroy();
-        }
-    }
-    /// <summary>
     /// Make an AI controlled aircraft unusable
     /// </summary> 
     /// <param name="CurPlayer"></param>
     /// <param name="ActorMain"></param>
     protected void DamagePlane(AiActor ActorMain, Player CurPlayer)
     {
-        if (DEBUG_MESSAGES) CLog.Write(ActorMain.Name() + " will damage/destroy aircraft left by player " + CurPlayer.Name());
-        foreach (AiActor actor in ActorMain.Group().GetItems())
+        if (DEBUG_MESSAGES) CLog.Write("Try to destroy aircraft" + ActorMain.Name() + " that was controlled by player " + CurPlayer.Name());
+        if (!(ActorMain is AiAircraft))
         {
-            if (actor == null || !(actor is AiAircraft))
-            {
-                return;
-            }
+            if (DEBUG_MESSAGES) CLog.Write("Damage/destroy " + ActorMain.Name() + " cancelled. ActorMain is NOT AiAircraft");
+            return;
+        }
 
-            AiAircraft Aircraft = (actor as AiAircraft);
+        AiAircraft Aircraft = (ActorMain as AiAircraft);
 
-            if (!IsAiControlledPlane(Aircraft))
-            {
-                if (DEBUG_MESSAGES) CLog.Write("Damage/destroy " + ActorMain.Name() + " cancelled. Player " + CurPlayer.Name() + " still in aircraft.");
-                return;
-            }
+        if (!IsAiControlledPlane(Aircraft))
+        {
+            if (DEBUG_MESSAGES) CLog.Write("Damage/destroy " + ActorMain.Name() + " cancelled. Player " + CurPlayer.Name() + " still in aircraft.");
+            return;
+        }
 
-            if (Aircraft == null)
-            {
-                return;
-            }
+        /// Make Damage
+        /// We wrap in try ... catch to make sure at least *some* of them are effected
+        /// no matter what happens (e.g. the Wing part throws on Blenheims)
 
-            /// Make Damage
-            /// We wrap in try ... catch to make sure at least *some* of them are effected
-            /// no matter what happens (e.g. the Wing part throws on Blenheims)
+        /// Damage named parts
+        try
+        {
+            Aircraft.hitNamed(part.NamedDamageTypes.ControlsElevatorDisabled);
+            Aircraft.hitNamed(part.NamedDamageTypes.ControlsAileronsDisabled);
+            Aircraft.hitNamed(part.NamedDamageTypes.ControlsRudderDisabled);
+            Aircraft.hitNamed(part.NamedDamageTypes.FuelPumpFailure);
+            Aircraft.hitNamed(part.NamedDamageTypes.Eng0TotalFailure);
+            Aircraft.hitNamed(part.NamedDamageTypes.ElecPrimaryFailure);
+            Aircraft.hitNamed(part.NamedDamageTypes.ElecBatteryFailure);
+        }
+        catch (Exception e)
+        {
+            if (DEBUG_MESSAGES) CLog.Write("Exception on damaging named parts: " + e.ToString());
+        }
 
-            /// Damage named parts
-            try
-            {
-                Aircraft.hitNamed(part.NamedDamageTypes.ControlsElevatorDisabled);
-                Aircraft.hitNamed(part.NamedDamageTypes.ControlsAileronsDisabled);
-                Aircraft.hitNamed(part.NamedDamageTypes.ControlsRudderDisabled);
-                Aircraft.hitNamed(part.NamedDamageTypes.FuelPumpFailure);
-                Aircraft.hitNamed(part.NamedDamageTypes.Eng0TotalFailure);
-                Aircraft.hitNamed(part.NamedDamageTypes.ElecPrimaryFailure);
-                Aircraft.hitNamed(part.NamedDamageTypes.ElecBatteryFailure);
-            }
-            catch (Exception e)
-            {
-                if (DEBUG_MESSAGES) CLog.Write("Exception on damaging named parts: " + e.ToString());
-            }
+        /// Damage wings
+        //try
+        //{
+        //    Aircraft.hitLimb(part.LimbNames.WingL1, -0.5);
+        //    Aircraft.hitLimb(part.LimbNames.WingL2, -0.5);
+        //    Aircraft.hitLimb(part.LimbNames.WingL3, -0.5);
+        //    Aircraft.hitLimb(part.LimbNames.WingL4, -0.5);
+        //    Aircraft.hitLimb(part.LimbNames.WingL5, -0.5);
+        //    Aircraft.hitLimb(part.LimbNames.WingL6, -0.5);
+        //    Aircraft.hitLimb(part.LimbNames.WingL7, -0.5);
+        //}
+        //catch(Exception e)
+        //{
+        //    if(DEBUG_MESSAGES) CLog.Write("Exception on damaging wings: "+e.ToString());
+        //}
 
-            /// Damage wings
-            //try
-            //{
-            //    Aircraft.hitLimb(part.LimbNames.WingL1, -0.5);
-            //    Aircraft.hitLimb(part.LimbNames.WingL2, -0.5);
-            //    Aircraft.hitLimb(part.LimbNames.WingL3, -0.5);
-            //    Aircraft.hitLimb(part.LimbNames.WingL4, -0.5);
-            //    Aircraft.hitLimb(part.LimbNames.WingL5, -0.5);
-            //    Aircraft.hitLimb(part.LimbNames.WingL6, -0.5);
-            //    Aircraft.hitLimb(part.LimbNames.WingL7, -0.5);
-            //}
-            //catch(Exception e)
-            //{
-            //    if(DEBUG_MESSAGES) CLog.Write("Exception on damaging wings: "+e.ToString());
-            //}
+        /// Damage engines
+        try
+        {
+            int iNumOfEngines = (Aircraft.Group() as AiAirGroup).aircraftEnginesNum();
 
-            /// Damage engines
-            try
+            for (int i = 0; i < iNumOfEngines; i++)
             {
-                int iNumOfEngines = (Aircraft.Group() as AiAirGroup).aircraftEnginesNum();
+                Aircraft.hitNamed((part.NamedDamageTypes)Enum.Parse(typeof(part.NamedDamageTypes), "Eng" + i.ToString() + "TotalFailure"));
+            }
+        }
+        catch (Exception e)
+        {
+            if (DEBUG_MESSAGES) CLog.Write("Exception on damageing engines: " + e.ToString());
+        }
 
-                for (int i = 0; i < iNumOfEngines; i++)
-                {
-                    Aircraft.hitNamed((part.NamedDamageTypes)Enum.Parse(typeof(part.NamedDamageTypes), "Eng" + i.ToString() + "TotalFailure"));
-                }
-            }
-            catch (Exception e)
-            {
-                if (DEBUG_MESSAGES) CLog.Write("Exception on damageing engines: " + e.ToString());
-            }
+        int iDestroyTimeout = TIMEOUT_ABANDONED;
+        if ( (m_Mission as Mission).IsAircraftAtFriendlyAirfield(Aircraft) )
+        {
+            iDestroyTimeout = TIMEOUT_ATFRIENDLYBASE;
+        }
 
-            double destroyTimeout = m_iSecondsUntilRemoveAbandoned;
-            bool atFriendlyAirfield = false;
-            bool aircraftIsOnTheGround = false;
-            if (!Aircraft.IsAirborne())
+        // Destroy time! ... maybe.
+        if (iDestroyTimeout > -1)
+        {
+            m_Mission.Timeout(iDestroyTimeout, () =>
             {
-                aircraftIsOnTheGround = true;
-                if (DEBUG_MESSAGES) CLog.Write(ActorMain.Name() + " is not airborne.");
-            }
-            else
-            {
-                double aircraftAGL = Aircraft.getParameter(part.ParameterTypes.Z_AltitudeAGL, -1);
-                double aircraftTAS = Aircraft.getParameter(part.ParameterTypes.Z_VelocityTAS, -1);
-                if (DEBUG_MESSAGES) CLog.Write(ActorMain.Name() + " is AGL=" + aircraftAGL.ToString()+"m and TAS=" + aircraftTAS.ToString() + "m/s");
-                if ((aircraftAGL < 5) && (aircraftTAS < 3.6))
-                {
-                    aircraftIsOnTheGround = true;
-                }
-            }
-
-            if (aircraftIsOnTheGround)
-            {
-                if (DEBUG_MESSAGES) CLog.Write(ActorMain.Name() + " is on the ground.");
-                Point3d aircraftPos = Aircraft.Pos();
-                int playerArmy = CurPlayer.Army();
-                AiAirport[] airports = m_Mission.GamePlay.gpAirports();
-                AiAirport airportFriendly, airportNeutral;
-                Point3d airportNeutralPos;
-                for (int i = 0; i < airports.Length; i++)
-                {
-                    airportNeutral = airports[i];
-                    // get neutral airfileds with friendly spawn area airports nearby...
-                    if (airportNeutral.Army() == 0)
-                    {
-                        airportNeutralPos = airportNeutral.Pos();
-                        for (int j = 0; j < airports.Length; j++)
-                        {
-                            if (i == j) continue;
-                            airportFriendly = airports[j];
-                            if (airportFriendly.Army() == playerArmy)
-                            {
-                                Point3d airportFriendlyPos = airportFriendly.Pos();
-                                if (airportNeutralPos.distanceLinf(ref airportFriendlyPos) < airportNeutral.CoverageR())
-                                {
-                                    // Ok, this neutral airport contain friendly spawn area airport. Check if we are in this neutral airport radius
-                                    double distToAirportNeutral = airportNeutralPos.distanceLinf(ref aircraftPos);
-                                    if (distToAirportNeutral < airportNeutral.CoverageR())
-                                    {
-                                        destroyTimeout = m_iSecondsUntilRemoveAtFriendlyBase;
-                                        if (DEBUG_MESSAGES) CLog.Write("Will destroy " + ActorMain.Name() + " instantly on friendly airfiled " + airportNeutral.Name() + " distance " + distToAirportNeutral.ToString());
-                                        atFriendlyAirfield = true;
-                                        m_Mission.Timeout(destroyTimeout, () =>
-                                        {
-                                            DestroyPlane(Aircraft);
-                                        });
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                        if (atFriendlyAirfield)
-                            break;
-                    }
-                }
-            }
-            else
-            {
-                if (DEBUG_MESSAGES) CLog.Write(ActorMain.Name() + " is airborne.");
-            }
-            if (!atFriendlyAirfield) {
-                if (DEBUG_MESSAGES) CLog.Write("Will not be destroy " + ActorMain.Name() + " due to abbandoned aircraft.");
-            }
+                DestroyPlane(Aircraft);
+            });
+        }
+    }
+    /// <summary>
+    /// Destroy aircraft
+    /// </summary> 
+    protected void DestroyPlane(AiAircraft Aircraft)
+    {
+        if (Aircraft != null)
+        {
+            if (DEBUG_MESSAGES) CLog.Write("DestroyPlane() : aircraft " + Aircraft.Name() + " to be destroyed right now.");
+            Aircraft.Destroy();
+        }
+        else
+        {
+            if (DEBUG_MESSAGES) CLog.Write("DestroyPlane() : no aircraft (Aircraft == null)");
         }
     }
     /// <summary>
@@ -233,7 +176,7 @@ public class CKillDisusedPlanes {
     /// </summary> 
     /// <param name="Aircraft"></param>
     /// <returns>true if no real player is in any of the plane's positions</returns>
-    protected bool IsAiControlledPlane(AiAircraft Aircraft)
+    public bool IsAiControlledPlane(AiAircraft Aircraft)
     {
         if (Aircraft == null)
         {
